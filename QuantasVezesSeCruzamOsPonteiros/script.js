5e96486938161c2b99ec32797cbe909a087a14cc
Luís Miguel da Rocha Alves <up201405308@fe.up.pt>
2020-06-16 16:03:27 +0100

    portuguese cv, tabs on main page

--- QuantasVezesSeCruzamOsPonteiros/script.js
+++ QuantasVezesSeCruzamOsPonteiros/script.js
@@ -1,8 +1,8 @@
 var c = document.getElementById("clock");
 var ctx = c.getContext("2d");
-var x = 125
-var y = 125
-var r = 100
+var x = 250
+var y = 250
+var r = 200
 var h = 0, m = 0, s = 0
 var tolerance = Math.PI / 360
 var textColor = 'black'
@@ -11,7 +11,7 @@ function initClock() {
 	ctx.clearRect(0, 0, c.width, c.height)
 	ctx.strokeStyle = textColor;
 	ctx.fillStyle = textColor;
-	ctx.font = "14px Ubuntu";
+	ctx.font = "28px Ubuntu";
 	ctx.beginPath();
 	ctx.arc(x, y, r, 0, 2 * Math.PI);
 	ctx.stroke();
@@ -23,9 +23,11 @@ function initClock() {
 		ctx.beginPath();
 		if (i % 5 == 0) {
 			if (i == 0)
-				ctx.fillText(12, x - 12 + 1.1 * r * Math.cos(2 * Math.PI * i / 60 - Math.PI / 2), y + 5 + 1.1 * r * Math.sin(2 * Math.PI * i / 60 - Math.PI / 2));
+				ctx.fillText(12, x - 16 + 1.1 * r * Math.cos(2 * Math.PI * i / 60 - Math.PI / 2), y + 10 + 1.1 * r * Math.sin(2 * Math.PI * i / 60 - Math.PI / 2));
+			else if (i > 45)
+				ctx.fillText(i / 5, x - 16 + 1.1 * r * Math.cos(2 * Math.PI * i / 60 - Math.PI / 2), y + 10 + 1.1 * r * Math.sin(2 * Math.PI * i / 60 - Math.PI / 2));
 			else
-				ctx.fillText(i / 5, x - 8 + 1.1 * r * Math.cos(2 * Math.PI * i / 60 - Math.PI / 2), y + 5 + 1.1 * r * Math.sin(2 * Math.PI * i / 60 - Math.PI / 2));
+				ctx.fillText(i / 5, x - 8 + 1.1 * r * Math.cos(2 * Math.PI * i / 60 - Math.PI / 2), y + 10 + 1.1 * r * Math.sin(2 * Math.PI * i / 60 - Math.PI / 2));
 			ctx.moveTo(x + 0.9 * r * Math.cos(2 * Math.PI * i / 60 - Math.PI / 2), y + 0.9 * r * Math.sin(2 * Math.PI * i / 60 - Math.PI / 2));
 		} else
 			ctx.moveTo(x + 0.95 * r * Math.cos(2 * Math.PI * i / 60 - Math.PI / 2), y + 0.95 * r * Math.sin(2 * Math.PI * i / 60 - Math.PI / 2));
@@ -42,9 +44,9 @@ function animateHands(h, m, s) {
 	ctx.beginPath();
 	ctx.arc(x, y, 0.89 * r, 0, 2 * Math.PI);
 	ctx.clip();
-	ctx.clearRect(0, 0, 250, 250);
+	ctx.clearRect(0, 0, 500, 500);
 	ctx.beginPath();
-	ctx.lineWidth = 2
+	ctx.lineWidth = 4
 	ctx.moveTo(x, y);
 	ctx.lineTo(x + 0.4 * r * Math.cos(2 * Math.PI * hang - Math.PI / 2), y + 0.4 * r * Math.sin(2 * Math.PI * hang - Math.PI / 2));
 	ctx.stroke();
@@ -53,7 +55,7 @@ function animateHands(h, m, s) {
 	ctx.lineTo(x + 0.6 * r * Math.cos(2 * Math.PI * mang - Math.PI / 2), y + 0.6 * r * Math.sin(2 * Math.PI * mang - Math.PI / 2));
 	ctx.stroke();
 	ctx.beginPath();
-	ctx.lineWidth = 1
+	ctx.lineWidth = 2
 	ctx.strokeStyle = "#FF0000";
 	ctx.moveTo(x, y);
 	ctx.lineTo(x + 0.85 * r * Math.cos(2 * Math.PI * sang - Math.PI / 2), y + 0.85 * r * Math.sin(2 * Math.PI * sang - Math.PI / 2));
@@ -71,29 +73,32 @@ function sleep(ms) {
 }
 
 async function next() {
-	document.getElementById("next").disabled = true
-	s++
-	if (s >= 60) {
-		m++
-		s = 0
-		if (m >= 60) {
-			h++
-			m = 0
-			if (h >= 24) {
-				h = 0
+	while (true) {
+		document.getElementById("next").disabled = true
+		s++
+		if (s >= 60) {
+			m++
+			s = 0
+			if (m >= 60) {
+				h++
+				m = 0
+				if (h >= 24) {
+					h = 0
+				}
 			}
 		}
-	}
-	angs = animateHands(h, m, s)
-	document.querySelector('#time').innerHTML = `${("00" + h).slice(-2)}h${("00" + m).slice(-2)}m${("00" + s).slice(-2)}s`
-	while (angs[0] >= 1)
-		angs[0] -= 1
-	if (Math.abs(angs[0] - angs[1]) < tolerance && Math.abs(angs[0] - angs[2]) < tolerance && Math.abs(angs[2] - angs[1]) < tolerance) {
-		document.getElementById("next").disabled = false
-		console.log(angs)
-	} else {
-		await sleep(1)
-		next()
+		angs = animateHands(h, m, s)
+		document.querySelector('#time').innerHTML = `${("00" + h).slice(-2)}h${("00" + m).slice(-2)}m${("00" + s).slice(-2)}s`
+		while (angs[0] >= 1)
+			angs[0] -= 1
+		if (Math.abs(angs[0] - angs[1]) < tolerance && Math.abs(angs[0] - angs[2]) < tolerance && Math.abs(angs[2] - angs[1]) < tolerance) {
+			document.getElementById("next").disabled = false
+			console.log(angs)
+			await sleep(5000)
+			//break
+		} else {
+			await sleep(1)
+		}
 	}
 }
 
